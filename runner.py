e1722cc8f4d35ec4a16385e8111a48c65f848f57
Atul Singh <atul23571@gmail.com>
2019-03-12 19:31:28 +0530

    updated with dict modification

--- runner.py
+++ runner.py
@@ -8,8 +8,10 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException,
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import os
+import re
 from flask import Flask
-from xvfbwrapper import Xvfb
+from collections import OrderedDict
+# from xvfbwrapper import Xvfb
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -17,15 +19,14 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def runner(selenium_file):
     print("reached inside runner")
-    print(selenium_file)
-    har_arr = {}
+    har_arr = OrderedDict()
     server = Server("./browsermob-proxy-2.1.4/bin/browsermob-proxy")
     print("included proxy")
     server.start()
     proxy = server.create_proxy()
     profile = webdriver.FirefoxProfile()
-    display = Xvfb()
-    display.start()
+    # display = Xvfb()
+    # display.start()
     profile.set_proxy(proxy.selenium_proxy())
     driver = webdriver.Firefox(firefox_profile=profile, executable_path='./geckodriver')
     # driver.set_page_load_timeout(20)
@@ -56,59 +57,65 @@ def runner(selenium_file):
         # fo.write(json.dumps(har_data))
         # fo.close()
     i = 0
-    driver.implicitly_wait(20)
-    for obj in data['tests'][0]['commands']:
-        i = i + 1
-        print("commands executed  " + str(i))
-        proxy.new_har("google", {"captureHeaders": True, "captureContent": True})
-        if obj["command"] == 'mouseOver':
-            try:
-                ele = driver.find_element(selector_map[obj['target'].split('=')[0]], obj['target'].split('=')[1])
-            except NoSuchElementException:
-                ele = driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1])
-            hover = ActionChains(driver).move_to_element(ele)
-            hover.perform()
-        if obj['command'] == 'click':
-            try:
-                element = driver.find_element(selector_map[obj['target'].split('=')[0]], obj['target'].split('=')[1])
-            except NoSuchElementException:
-                print("first failed to click")
-                element = driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1])
-            file_name = element.get_attribute('innerHTML')
-            try:
-                element.click()
-            except ElementClickInterceptedException:
-                driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]],
-                                    obj['targets'][-1][0].split('=')[1]).click()
-            har_data = proxy.har
-            if len(har_data['log']['entries']) > 0:
-                har_arr[file_name] = har_data
-                # fo = open("./" + directory + "/" + obj['target'].split('=')[1] + ".har", "w")
-                # fo.write(json.dumps(har_data))
-                # fo.close()
-        if obj['command'] == 'type':
-            try:
-                driver.find_element(selector_map[obj['target'].split('=')[0]],obj['target'].split('=')[1]).send_keys(obj['value'])
-            except NoSuchElementException:
-                driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1]).send_keys(
-                    obj['value'])
-        if obj['command'] == 'select' or obj['command'] == 'addSelection':
-            try:
-                select = Select(driver.find_element(selector_map[obj['target'].split('=')[0]],obj['target'].split('=')[1]))
-            except NoSuchElementException:
-                select = Select(
-                    driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1]))
-            select.select_by_visible_text(obj['value'].split('=')[1])
-        if obj['command'] == 'sendKeys':
-            try:
-                driver.find_element(selector_map[obj['target'].split('=')[0]],obj['target'].split('=')[1]).send_keys(Keys.ENTER)
-            except NoSuchElementException:
-                driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1]).send_keys(
-                    Keys.ENTER)
+    try:
+        for obj in data['tests'][0]['commands']:
+            i = i + 1
+            print("commands executed  " + str(i))
+            proxy.new_har("google", {"captureHeaders": True, "captureContent": True})
+            if obj["command"] == 'mouseOver':
+                try:
+                    ele = driver.find_element(selector_map[obj['target'].split('=')[0]], obj['target'].split('=')[1])
+                except NoSuchElementException:
+                    ele = driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1])
+                hover = ActionChains(driver).move_to_element(ele)
+                hover.perform()
+            if obj['command'] == 'click':
+                try:
+                    element = driver.find_element(selector_map[obj['target'].split('=')[0]], obj['target'].split('=')[1])
+                except NoSuchElementException:
+                    print("first failed to click")
+                    element = driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1])
+                file_name = element.get_attribute('innerHTML')
+                if file_name == '' or not re.match(r'^\w+$', file_name.replace(" ", '_')):
+                    file_name = 'Step_' + str(i)
 
-
+                try:
+                    element.click()
+                except ElementClickInterceptedException:
+                    driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]],
+                                        obj['targets'][-1][0].split('=')[1]).click()
+                har_data = proxy.har
+                if len(har_data['log']['entries']) > 0:
+                    har_arr[file_name] = har_data
+                    # fo = open("./" + directory + "/" + obj['target'].split('=')[1] + ".har", "w")
+                    # fo.write(json.dumps(har_data))
+                    # fo.close()
+            if obj['command'] == 'type':
+                try:
+                    driver.find_element(selector_map[obj['target'].split('=')[0]],obj['target'].split('=')[1]).send_keys(obj['value'])
+                except NoSuchElementException:
+                    driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1]).send_keys(
+                        obj['value'])
+            if obj['command'] == 'select' or obj['command'] == 'addSelection':
+                try:
+                    select = Select(driver.find_element(selector_map[obj['target'].split('=')[0]],obj['target'].split('=')[1]))
+                except NoSuchElementException:
+                    select = Select(
+                        driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1]))
+                select.select_by_visible_text(obj['value'].split('=')[1])
+            if obj['command'] == 'sendKeys':
+                try:
+                    driver.find_element(selector_map[obj['target'].split('=')[0]],obj['target'].split('=')[1]).send_keys(Keys.ENTER)
+                except NoSuchElementException:
+                    driver.find_element(selector_map[obj['targets'][-1][0].split('=')[0]], obj['targets'][-1][0].split('=')[1]).send_keys(
+                        Keys.ENTER)
+    except Exception as e:
+        server.stop()
+        driver.quit()
+        # display.stop()
+        return {"status":"failed", "message": "Something Went wrong", "error": e}
     server.stop()
     print("har files generated")
     driver.quit()
-    display.stop()
-    return har_arr
+    # display.stop()
+    return {"status":"success", "hars": dict(har_arr)}
